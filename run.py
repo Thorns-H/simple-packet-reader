3ca6b104ffc6a0560e5e3fb179a55f3955033406
Thorns-H <abrahamm9986@gmail.com>
2022-02-16 10:26:42 -0600

    Renamed script file

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
 from helpers import read_file, ethernet_frame
 import os
+import platform
 
 TESTING = "test_files"
 PATH = os.getcwd()
+if platform.system() != 'Linux':
+    CLEAR = 'cls'
+else:
+    CLEAR = 'clear'
 
 def main():
     while True:
         try:
-            os.system('clear')
+            os.system(CLEAR)
             os.system(f'cd && cd {PATH}/{TESTING} && ls -t > ../files.txt')
 
             with open('files.txt') as f:
@@ -20,7 +25,7 @@ def main():
 
             print('\n\t@Press CTRL + C to leave!')
 
-            file = int(input('\n   Selección: '))
+            file = int(input('\n\tSelect: '))
 
             if file >= len(data) or file < 0:
                 print(f'\n\t@ERROR: File with index "{file}" does not exists!')
@@ -29,6 +34,8 @@ def main():
                 ethernet_frame(read_file(f'{TESTING}/{data[file][:-1]}'), f'{data[file][:-1]}')  
         except KeyboardInterrupt:
             break
+        
+    os.system(CLEAR)
 
 if __name__ == '__main__':
     main()
